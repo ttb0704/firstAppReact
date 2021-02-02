0f0691fe3b07f62a8f89a3b5aca7835e52f28fcf
ttb0704 <ttb0704@naver.com>
2021-02-03 05:53:57 +0900

    컴포넌트/JSX/props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
+// function Food(props){
+//   console.log(props);
+//   return <h1>I like {props.fav}</h1>
+// }
+
+function Food({fav}){
+  console.log({fav});
+  return <h1>I like {fav}</h1>
+}
+
 function App() {
   //return <div className="App" />;
-  return <div>Hello React!</div>
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Food fav="kimchi"/>
+      <Food fav="samgiopsal"/>
+      <Food fav="chukumi"/>
+    </div>
+  );
 }
 
 export default App;
