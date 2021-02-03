9588f0417fbb73d4f67f5ac91bf5b15a13761dd8
ttb0704 <ttb0704@naver.com>
2021-02-04 06:17:17 +0900

    loading...

--- src/App.js
+++ src/App.js
@@ -2,59 +2,26 @@ import React from 'react';
 
 
 class App extends React.Component{
-
-  state ={
-    count:0,
-  }
-
-  add = () => {
-    console.log("add");
-    //this.state.count=1;
-    //this.setState({count:1});
-    //this.setState({count:this.state.count+1});
-    this.setState( current => ({
-      count: current.count+1,
-    }) );
-  };
-
-  minus = () => {
-    console.log("minus");
-    //this.state.count=-1;
-    //this.setState({count:-1});
-    //this.setState({count:this.state.count-1});
-    this.setState( current => ({
-      count: current.count-1,
-    }) );
-  };
-
-  render(){
-    console.log('render');
-    return (
-      <div>
-        <h1>the number {this.state.count}</h1>
-        <button onClick={this.add} >Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
-  }
-
-  constructor(props){
-    super(props);
-    console.log('constructor');
+  state={
+    isLoading:true,
+    movie:[],
   }
 
   componentDidMount(){
-    console.log('componentDidMount');
+    //영화 데이터 로딩이 완료 되면!
+    setTimeout( ()=>{
+      this.setState({isLoading:false});
+    },6000 );
   }
 
-  componentDidUpdate(){
-    console.log('componentDidUpdate');
+  render(){
+    const {isLoading}=this.state;
+    return(
+      <div>
+        {isLoading ? 'Loaging...':'we are ready'}
+      </div>
+    )
   }
-  
-  componemtWillUnmount(){
-    console.log('componemtWillUnmount');
-  }
-
 }
 
 export default App;
