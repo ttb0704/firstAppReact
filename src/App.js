d679dfa5a18e24d1b5fb5b286dd06def46c57499
ttb0704 <ttb0704@naver.com>
2021-02-05 06:30:46 +0900

    movieApp basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css'
 
 class App extends React.Component{
   state={
     isLoading:true,
-    movie:[],
+    movies:[],
+  }
+
+  //https://ko.javascript.info/
+  //https://ko.javascript.info/async-await
+  getMovies = async () => {
+    const{
+      data:{
+        data:{movies}
+      }
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    // console.log(movies);
+    this.setState({movies:movies, isLoading:false});
   }
 
   componentDidMount(){
     //영화 데이터 로딩이 완료 되면!
-    setTimeout( ()=>{
+    /* setTimeout( ()=>{
       this.setState({isLoading:false});
-    },6000 );
+    },6000 ); */
+
+    // axios로 API 호출
+    //axios.get('https://yts.mx/api/v2/list_movies.json');
+    this.getMovies();
   }
 
   render(){
-    const {isLoading}=this.state;
+    const {isLoading,movies}=this.state;
     return(
-      <div>
-        {isLoading ? 'Loaging...':'we are ready'}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">'Loaging...'</span>
+          </div>
+          ):(
+            <div class="movies">
+              {movies.map (movie =>(
+               <Movie 
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
+            </div>
+          )}
+      </section>
     )
   }
 }
